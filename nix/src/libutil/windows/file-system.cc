4377edef35d8767361758c677f504ce911729e78
awfixer <git@awfixer.me>
2025-12-28 21:54:45 -0800

    update nix to add both

---
+++ nix/src/libutil/windows/file-system.cc
@@ -0,0 +1,56 @@
+#include "nix/util/file-system.hh"
+#include "nix/util/windows-error.hh"
+#include "nix/util/logging.hh"
+
+namespace nix {
+
+using namespace nix::windows;
+
+void setWriteTime(
+    const std::filesystem::path & path, time_t accessedTime, time_t modificationTime, std::optional<bool> optIsSymlink)
+{
+    // FIXME use `std::filesystem::last_write_time`.
+    //
+    // Would be nice to use std::filesystem unconditionally, but
+    // doesn't support access time just modification time.
+    //
+    // System clock vs File clock issues also make that annoying.
+    warn("Changing file times is not yet implemented on Windows, path is %s", path);
+}
+
+Descriptor openDirectory(const std::filesystem::path & path)
+{
+    return CreateFileW(
+        path.c_str(),
+        GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_FLAG_BACKUP_SEMANTICS,
+        NULL);
+}
+
+std::filesystem::path defaultTempDir()
+{
+    wchar_t buf[MAX_PATH + 1];
+    DWORD len = GetTempPathW(MAX_PATH + 1, buf);
+    if (len == 0 || len > MAX_PATH)
+        throw WinError("getting default temporary directory");
+    return std::filesystem::path(buf);
+}
+
+void deletePath(const std::filesystem::path & path)
+{
+    std::error_code ec;
+    std::filesystem::remove_all(path, ec);
+    if (ec && ec != std::errc::no_such_file_or_directory)
+        throw SysError(ec.default_error_condition().value(), "recursively deleting %1%", path);
+}
+
+void deletePath(const std::filesystem::path & path, uint64_t & bytesFreed)
+{
+    bytesFreed = 0;
+    deletePath(path);
+}
+
+} // namespace nix
